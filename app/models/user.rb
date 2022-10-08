1bf6aa00b68835f2a8797f9a0e0de4d860a0907d
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2022-10-08 12:12:40 +0100

    User validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-  has_many :article
+  # has_many :article
+  validates :user, presence: true, 
+                    length: { minimum: 3, maximum: 16 }, 
+                    uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,  
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 16 },
+                    format: { with: VALID_EMAIL_REGEX } 
+
 end
