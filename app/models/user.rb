c887dfc8ae7fe171b79c2d9c00168aa0714c81e7
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2022-10-08 15:43:39 +0100

    Sign up added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,  
                     uniqueness: { case_sensitive: false },
-                    length: { maximum: 16 },
+                    length: { maximum: 100},
                     format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true,  
+                    length: { minimum: 8, maximum: 16 }
   has_secure_password
 end
