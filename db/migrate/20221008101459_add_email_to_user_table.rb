b88b8cd392218183fd010e5cbd60b2d9057a6002
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2022-10-08 11:20:20 +0100

    User table added

---
+++ db/migrate/20221008101459_add_email_to_user_table.rb
@@ -0,0 +1,5 @@
+class AddEmailToUserTable < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_30_202639) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_08_101459) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
